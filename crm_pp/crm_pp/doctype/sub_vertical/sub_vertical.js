1d4cc5d75b9c02e7aa3945593206f413f37ec0f1
Himanshu Shivhare <himanshushivhare047@mail.com>
2025-07-15 21:30:14 +0530

    doctypes created

---
+++ crm_pp/crm_pp/doctype/sub_vertical/sub_vertical.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Octavision Software Solutions and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Sub Vertical", {
+// 	refresh(frm) {
+
+// 	},
+// });
